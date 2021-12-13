09fa8678fdd2949a89403ffd0a7b565a3e44b722
farazianna <farazianna09@gmail.com>
2021-12-13 07:27:54 +0000

    Done

--- video.py
+++ video.py
@@ -1,7 +1,7 @@
 import picamera
 from time import sleep
-
-with picamera.Picamera() as camera:
+#setup the camera
+with picamera.PiCamera() as camera:
      camera.start_recording("Video_Group1.h264")
      sleep(5)
      camera.stop_recording()
