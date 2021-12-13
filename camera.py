fc4eccb10c43f65144885a8e4be417df12fcfb82
adzwinah <Adzwinahbt.main@gmail.com>
2021-12-13 14:08:03 +0800

    Done

--- camera.py
+++ camera.py
@@ -4,5 +4,6 @@ print ("Ready Guys.")
 with picamera.PiCamera() as Camera:
      camera.start_preview()
      camera.resolution = (1280, 720)
+     camera.capture("/home/pi/Desktop/project/")
      camera.stop_preview()
      print("Take picture Olready")
